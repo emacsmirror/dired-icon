6483db4cc4fabb7f2367009c816f58a392ada74a
Hong Xu <hong@topbug.net>
2016-10-17 12:55:09 -0700

    A better detection of GTK2.

--- dired-icon.el
+++ dired-icon.el
@@ -49,8 +49,8 @@
   "The path of the executable of the \"file\" executable."
   :type 'string)
 
-(defcustom dired-icon-python-executable "python2"
-  "The path of the executable of the \"python\" executable (Python 2.x is required)."
+(defcustom dired-icon-python2-executable "python2"
+  "The path of the executable of the \"python2\" executable (Python 2.x is required)."
   :type 'string)
 
 (defvar dired-icon--script-directory
@@ -64,37 +64,44 @@
 
 (defun dired-icon--get-icons (file-names)
   "Create an alist, which maps the files FILE-NAMES to image objects."
-  (cond ((member system-type '(windows-nt darwin))  ;; to be implemented
-         (cl-pairlis file-names
-                     (make-list (length file-names) nil)))
-        (t   ;; X11
-         (with-temp-buffer
-           ;; insert the list of mimetypes into the temp buffer
-           (dolist (fn file-names)
-             (goto-char (point-min)) ;; reverse the file name insertion order
-             (call-process dired-icon-file-executable nil t nil
-                           "-b" "--mime-type" fn))
-           ;; replace the current buffer with an icon file name in each line
-           (call-process-region (point-min) (point-max)
-                                dired-icon-python-executable
-                                t t nil
-                                (expand-file-name
-                                 "get-icon-path.py"
-                                 dired-icon--script-directory))
-           ;; create an image object for each icon
-           (let ((icon-images nil))
-             (dolist (icon-fname (split-string (buffer-string) "\n" nil))
-               (if (string= icon-fname "")
-                   (push nil icon-images)
-                 (let ((image (gethash icon-fname dired-icon--image-hash)))
-                   (unless image
-                     (setq image (create-image icon-fname))
-                     (puthash icon-fname image dired-icon--image-hash))
-                   (push image icon-images))))
-             ;; The first element is an nil caused by the file end \n. Remove
-             ;; it.
-             (pop icon-images)
-             (cl-pairlis file-names icon-images))))))
+  (cond
+   ;; GTK2
+   ((and (executable-find dired-icon-file-executable)
+         (executable-find dired-icon-python2-executable)
+         (= 0 (call-process dired-icon-python2-executable nil nil nil
+                            (expand-file-name
+                             "get-icon-path-gtk2.py"
+                             dired-icon--script-directory) "test")))
+    (with-temp-buffer
+      ;; insert the list of mimetypes into the temp buffer
+      (dolist (fn file-names)
+        (goto-char (point-min)) ;; reverse the file name insertion order
+        (call-process dired-icon-file-executable nil t nil
+                      "-b" "--mime-type" fn))
+      ;; replace the current buffer with an icon file name in each line
+      (call-process-region (point-min) (point-max)
+                           dired-icon-python2-executable
+                           t t nil
+                           (expand-file-name
+                            "get-icon-path-gtk2.py"
+                            dired-icon--script-directory))
+      ;; create an image object for each icon
+      (let ((icon-images nil))
+        (dolist (icon-fname (split-string (buffer-string) "\n" nil))
+          (if (string= icon-fname "")
+              (push nil icon-images)
+            (let ((image (gethash icon-fname dired-icon--image-hash)))
+              (unless image
+                (setq image (create-image icon-fname))
+                (puthash icon-fname image dired-icon--image-hash))
+              (push image icon-images))))
+        ;; The first element is an nil caused by the file end \n. Remove
+        ;; it.
+        (pop icon-images)
+        (cl-pairlis file-names icon-images))))
+   (t  ;; other unsupported systems
+    (cl-pairlis file-names
+                (make-list (length file-names) nil)))))
 
 (defun dired-icon-display ()
   "Display the icons."
--- get-icon-path-gtk2.py
+++ get-icon-path-gtk2.py
@@ -7,6 +7,10 @@ import sys
 import gio
 import gtk
 
+# If we only need to test the availability of gtk libraries, exit now.
+if len(sys.argv) == 2 and sys.argv[1] == 'test':
+    sys.exit(0)
+
 for line in sys.stdin:
     # adjusted from
     # http://stackoverflow.com/questions/21944166/get-the-default-icon-for-specific-file-type-in-ubuntu-os-by-python/21947530#21947530
